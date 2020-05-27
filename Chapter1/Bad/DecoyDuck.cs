fb8591e75a343552c03931e8e7c810ef00882a98
mouse71rus <zhimerin98007@gmail.com>
2020-05-27 13:20:55 +0300

    Добавлены утки-приманки

---
+++ Chapter1/Bad/DecoyDuck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter1.Bad
+{
+    public class DecoyDuck : Duck
+    {
+        public new void quack()
+        {
+            //Пустое переопределение, чтобы приманки не крякали
+        }
+
+        public new void fly()
+        {
+            //Пустое переопределение, чтобы приманки не летали
+        }
+
+        public override void display()
+        {
+            // Конкретная версия для DecoyDuck
+        }
+    }
+}
