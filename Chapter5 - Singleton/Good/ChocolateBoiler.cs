556053440efa80d1b7529ab555cdd409fb3a4401
mouse71rus <zhimerin98007@gmail.com>
2020-06-04 13:36:19 +0300

    Singleton

---
+++ Chapter5 - Singleton/Good/ChocolateBoiler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter5.Good
+{
+    public class ChocolateBoiler
+    {
+        private bool Empty;
+        private bool Boiled;
+
+        private static ChocolateBoiler Instance;
+
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            return Instance ?? new ChocolateBoiler();
+        }
+
+        public void Fiil()
+        {
+            if (IsEmpty())
+            {
+                // Заполнение нагревателя молочно-шоколадной смесью
+                Empty = false;
+                Boiled = false;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return Empty;
+        }
+
+        public bool IsBoiled()
+        {
+            return Boiled;
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty() && IsBoiled())
+            {
+                // Слить нагретое молоко и шоколад
+                Empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty() && !IsBoiled())
+            {
+                // Слить нагретое молоко и шоколад
+                Boiled = true;
+            }
+        }
+    }
+}
