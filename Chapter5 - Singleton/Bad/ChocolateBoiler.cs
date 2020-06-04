4f706b9a61c4238b3592c7a4c09fee42b586561f
mouse71rus <zhimerin98007@gmail.com>
2020-06-04 13:31:54 +0300

    Add ChocolateBoiled class

---
+++ Chapter5 - Singleton/Bad/ChocolateBoiler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter5.Bad
+{
+    public class ChocolateBoiler
+    {
+        private bool Empty;
+        private bool Boiled;
+
+        public ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public void Fiil()
+        {
+            if(IsEmpty())
+            {
+                // Заполнение нагревателя молочно-шоколадной смесью
+                Empty = false;
+                Boiled = false;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return Empty;
+        }
+
+        public bool IsBoiled()
+        {
+            return Boiled;
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty() && IsBoiled())
+            {
+                // Слить нагретое молоко и шоколад
+                Empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty() && !IsBoiled())
+            {
+                // Слить нагретое молоко и шоколад
+                Boiled = true;
+            }
+        }
+    }
+}
