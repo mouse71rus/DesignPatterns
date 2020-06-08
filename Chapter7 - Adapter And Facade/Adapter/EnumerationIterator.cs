899414f7b6b39db74837458418ce690f4c73fc6e
mouse71rus <zhimerin98007@gmail.com>
2020-06-08 20:27:18 +0300

    Реальное использование адаптера

---
+++ Chapter7 - Adapter And Facade/Adapter/EnumerationIterator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter7.Adapter
+{
+    public class EnumerationIterator : IIterator
+    {
+        private IEnumeration enumeration;
+
+        public EnumerationIterator(IEnumeration enumeration)
+        {
+            this.enumeration = enumeration;
+        }
+
+        public bool HasNext()
+        {
+            return enumeration.HasMoreElements();
+        }
+
+        public object Next()
+        {
+            return enumeration.NextElements();
+        }
+
+        public void remove(object el)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
