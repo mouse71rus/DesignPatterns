fc8e5aeceb5231ca72c5b61d22cefe7d400f1fd7
mouse71rus <zhimerin98007@gmail.com>
2020-06-10 12:33:38 +0300

    Bad implements

---
+++ Chapter8 - Template Method/Bad/Coffee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter8.Bad
+{
+    public class Coffee
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            BrewCoffeeGrinds();
+            PourInCup();
+            AddSugarAndMilk();
+        }
+
+        public void AddSugarAndMilk()
+        {
+            Console.WriteLine("Adding Suger & Milk");
+        }
+
+        public void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        public void BrewCoffeeGrinds()
+        {
+            Console.WriteLine("Dripping Coffee through filter");
+        }
+
+        public void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+    }
+}
---
+++ Chapter8 - Template Method/Bad/Tea.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter8.Bad
+{
+    public class Tea
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            SteepTeaBag();
+            PourInCup();
+            AddLemon();
+        }
+
+        private void AddLemon()
+        {
+            Console.WriteLine("Adding Lemon");
+        }
+
+        private void SteepTeaBag()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+
+        public void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        public void BoilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+    }
+}
