7e0ecadde67978c02d51255de50e5303b7c32698
mouse71rus <zhimerin98007@gmail.com>
2020-06-10 13:11:42 +0300

    Template Method

---
+++ Chapter8 - Template Method/Good/Duck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter8.Good
+{
+    public class Duck : IComparable
+    {
+        public string Name;
+        public int Weight;
+
+        public Duck(string name, int weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Duck duck = obj as Duck;
+            if(this.Weight > duck.Weight)
+            {
+                return 1;
+            }
+            else if (this.Weight < duck.Weight)
+            {
+                return -1;
+            }
+            else
+            { // this.Weight == duck.Weight
+                return 0;
+            }
+        }
+    }
+}
--- Chapter8 - Template Method/Program.cs
+++ Chapter8 - Template Method/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Chapter8.Good;
 
 namespace Chapter8
 {
@@ -22,7 +23,41 @@ namespace Chapter8
             Console.WriteLine("Coffee");
             coffee.PrepareRecipe();
 
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Sort:");
+            Console.WriteLine();
+            Good.Duck[] ducks = new Good.Duck[] {
+                new Good.Duck("Howard", 5),
+                new Good.Duck("Marry", 7),
+                new Good.Duck("Andrew", 6),
+                new Good.Duck("Sergey", 4),
+                new Good.Duck("Max", 8),
+                new Good.Duck("Sashko", 2),
+                new Good.Duck("Forward", 7),
+            };
+            Console.WriteLine("Before: ");
+            Console.WriteLine();
+            display(ducks);
+
+            Console.WriteLine();
+            Console.WriteLine("After: ");
+            Console.WriteLine();
+
+            Array.Sort(ducks);
+            display(ducks);
+            Console.WriteLine("--------------------------");
+
             Console.ReadKey();
+        }
+
+        private static void display(Duck[] ducks)
+        {
+            foreach (var item in ducks)
+            {
+                Console.WriteLine($"{item.Name} {item.Weight}");
+            }
         }
     }
 }
