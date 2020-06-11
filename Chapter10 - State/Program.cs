33c5f8169bcc780608e85202d60c15156a2d7c81
Sergey Zhimerin <szhimerin@bng-it.ru>
2020-06-11 19:25:53 +0300

    Add new project

---
+++ Chapter10 - State/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Chapter10 - State");
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
